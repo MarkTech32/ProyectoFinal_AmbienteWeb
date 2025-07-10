52cd066dabd00731dbf7b46f9c7a79a290f98a57
kiory <100336613+kioryzou@users.noreply.github.com>
2025-07-10 13:22:02 -0600

    Se agrego la pagina de servicios con 6 servicios de ejemplo, se creo el archivo de servicios.css y se mantuvo el  diseño de la pagina de tutorias en servicios.                                                  Ademas se agreo una modificacion en servicios.css para mantener los botones en la parte de abajo de la tarjeta siempre y mantener un estilo profesional.

--- TutoX/app/views/componentes/navbar.php
+++ TutoX/app/views/componentes/navbar.php
@@ -3,7 +3,7 @@
             <a href="index.php" class="logo">TutoX</a>
             <ul class="menu-navegacion">
                 <li><a href="tutorias.php">Tutorías</a></li> <!-- Aqui hay que poner el link del php de tutorias -->
-                <li><a href="#servicios">Servicios</a></li> <!-- Aqui hay que poner el link del php de servicios -->
+                <li><a href="servicios.php">Servicios</a></li> <!-- Aqui hay que poner el link del php de servicios -->
                 <li><a href="#comunidad">Comunidad</a></li> <!-- Aqui hay que poner el link del php de comunidad -->
                 <li><a href="#perfil">Mi Perfil</a></li> <!-- Aqui hay que poner el link del php de perfil -->
             </ul>
